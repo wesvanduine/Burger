5bcb3e3a286553dd1c9093c285134ea6f04a24ac
Wes Van Duine <wesvanduine@gmail.com>
2017-07-27 17:49:34 -0700

    update and put

--- config/orm.js
+++ config/orm.js
@@ -6,6 +6,12 @@ var orm = {
 			if(err) throw err;
 			cb(result)
 		})
+	},
+	update: function(tableInput, condition, cb) {
+		connection.query('UPDATE '+tableInput+' SET devoured=true WHERE id='+condition+';', function(err, result) {
+			if(err) throw err;
+			cb(result);
+		})
 	}
 }
 
